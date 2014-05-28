a396c9c27428805291086f8c1b4dd391d0c86317
Josh Tate <joshtate04@me.com>
2014-05-27 21:54:44 -0400

    no message

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module MalimarTv
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
     config.assets.precompile += %w( .svg .eot .woff .ttf)
     
+    config.assets.compile = true
+    config.assets.precompile =  ['*.js', '*.css', '*.css.erb'] 
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
