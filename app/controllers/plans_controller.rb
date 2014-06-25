850a97bfd40a2ba1dea94144fd7c1c59394b3430
Josh Tate <jtate@variationmedia.com>
2014-06-25 15:00:42 -0400

    no message

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -18,7 +18,11 @@ class PlansController < ApplicationController
 		@plan.features = features
 
 		if @plan.save
-			update = AdminActivity.create(admin_id: current_admin.id, data: YAML.dump({type: 'Plan Change', message: "#{current_admin.name} updated a plan.", plan_id: @plan.id, old_plan_details: old_plan_details, new_plan_details: {name: @plan.name, months: @plan.months, price: @plan.price, features: YAML.load(@plan.features)}}))
+			unless @plan.features.nil?
+				update = AdminActivity.create(admin_id: current_admin.id, data: YAML.dump({type: 'Plan Change', message: "#{current_admin.name} updated a plan.", plan_id: @plan.id, old_plan_details: old_plan_details, new_plan_details: {name: @plan.name, months: @plan.months, price: @plan.price, features: YAML.load(@plan.features)}}))
+			else
+				update = AdminActivity.create(admin_id: current_admin.id, data: YAML.dump({type: 'Plan Change', message: "#{current_admin.name} updated a plan.", plan_id: @plan.id, old_plan_details: old_plan_details, new_plan_details: {name: @plan.name, months: @plan.months, price: @plan.price, features: nil}}))
+			end
 		end
 	end
 end
