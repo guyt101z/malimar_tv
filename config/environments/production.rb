c0f7470153c94606f57de9b3989e99e7b3f3428c
Josh Tate <jtate@variationmedia.com>
2014-08-25 05:39:03 -0400

    no message

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = true
-  config.force_ssl = true
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
