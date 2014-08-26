90c351d473c8659bb51bc89f63f3a8fa29d23ea3
Josh Tate <jtate@variationmedia.com>
2014-08-26 12:40:23 -0400

    no message

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
         post 'remote_sign_in_for_video' => 'videos#remote_sign_in_for_video'
         post 'remote_sign_in' => 'users#remote_sign_in'
 
-        post 'subscribe_newsletter_footer' => 'users#subscribe_newsletter_footer'
+        get 'subscribe_newsletter_footer' => 'users#subscribe_newsletter_footer'
   	# User Methods
     authenticate :user do
   	  	get 'account' => 'users#account'
