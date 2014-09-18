92f1f2947269c6448680cf11c244d09167cdbbce
Josh Tate <jtate@variationmedia.com>
2014-09-18 18:44:19 -0400

    no message

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,7 +24,7 @@
 //= require dataTables_bootstrap
 //= require jquery-fileupload/basic
 //= require jquery-fileupload/vendor/tmpl
-//= require
+//= require slick
 /**
  * Timeago is a jQuery plugin that makes it easy to support automatically
  * updating fuzzy timestamps (e.g. "4 minutes ago" or "about 1 day ago").
