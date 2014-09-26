399c2c251badc9e378b81fa29f165abe3a12d946
Josh Tate <jtate@variationmedia.com>
2014-09-26 17:43:33 -0400

    no message

--- db/migrate/20140829205432_remove_expiry_from_users.rb
+++ db/migrate/20140829205432_remove_expiry_from_users.rb
@@ -1,6 +1,4 @@
 class RemoveExpiryFromUsers < ActiveRecord::Migration
   def change
-      remove_column :users, :expiry
-      remove_column :devices, :expiry
   end
 end
--- db/migrate/20140906153218_add_sub_data_to_users.rb
+++ db/migrate/20140906153218_add_sub_data_to_users.rb
@@ -1,6 +1,5 @@
 class AddSubDataToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :start_date, :date
     add_column :users, :expiry, :date
   end
 end
